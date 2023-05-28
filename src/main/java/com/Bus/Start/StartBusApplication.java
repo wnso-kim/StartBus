a450c3a5eab3cafa7c6f0846714ae56e3b1bbc5a
wnso <wnso.kim@gmail.com>
2023-05-28 21:44:42 +0900

    first commit

---
+++ src/main/java/com/Bus/Start/StartBusApplication.java
@@ -0,0 +1,13 @@
+package com.Bus.Start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartBusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Bus/Start/StartBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bus.Start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
